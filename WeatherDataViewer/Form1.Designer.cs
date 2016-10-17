3bf36c39763f9535d7afc0a46eb81bf0645391cd
Sean Whitesell <Sean Whitesell>
2016-10-16 19:52:21 -0500

    Initial version showing bad code.

---
+++ WeatherDataViewer/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿using System.Threading;
+
+namespace WeatherDataViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // tbData
+            // 
+            this.tbData.Location = new System.Drawing.Point(12, 107);
+            this.tbData.Multiline = true;
+            this.tbData.Name = "tbData";
+            this.tbData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbData.Size = new System.Drawing.Size(630, 160);
+            this.tbData.TabIndex = 0;
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(567, 61);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(75, 23);
+            this.btnGetData.TabIndex = 1;
+            this.btnGetData.Text = "Get Data";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(548, 88);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 279);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.tbData);
+            this.Name = "Form1";
+            this.Text = "My Weather Data Go-Getter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Panel panel1;
+    }
+
+    public class WebClient
+    {
+        public string DownloadString(string command)
+        {
+            Thread.Sleep(5000);
+            return new System.Net.WebClient().DownloadString(command);
+        }
+    }
+}
+
